1d87bf020ce6383a5f94c7d39def0a76943654ba
arafat33498-prog <arafat33598@gmail.com>
2026-03-07 01:18:31 +0600

    add issue header with dynamic count and status indicators

--- script/load.js
+++ script/load.js
@@ -14,14 +14,19 @@ const loadAllIssues = () => {
         const issuesCard = document.getElementById("issues-card")
         issuesCard.innerHTML = ""
 
+        document.getElementById('issue-count').innerText = issues.length;
+
         issues.forEach(issue => {
 
             const statusIcon = issue.status === 'open' ? 'assets/open-Status.png' : 'assets/Closed- Status .png';
-            const card = `
-       <div class="card bg-white shadow-sm border border-gray-100 rounded-xl flex flex-col h-full hover:shadow-md transition-all">
+        
+        const card = document.createElement('div');
+        
+       
+        card.className = "card bg-white shadow-sm border border-gray-100 rounded-xl flex flex-col h-full hover:shadow-md transition-all";
+
+        card.innerHTML = `
             <div class="p-6 flex flex-col h-full">
-                
-                
                 <div class="flex justify-between items-center mb-4">
                     <div class="flex items-center gap-2">
                         <img src="${statusIcon}" alt="status" class="w-5 h-5 object-contain"> 
@@ -35,12 +40,10 @@ const loadAllIssues = () => {
                     ${issue.title}
                 </h2>
 
-               
                 <p class="text-gray-400 text-[11px] line-clamp-2 mb-5 leading-relaxed">
                     ${issue.description}
                 </p>
 
-              
                 <div class="flex gap-2 mb-8">
                     <span class="flex items-center gap-1 px-3 py-1 rounded-full bg-red-50 text-red-400 text-[9px] font-bold border border-red-100">
                          <img src="assets/BugDroid.png" alt="bug" class="w-3 h-3"> BUG
@@ -50,7 +53,6 @@ const loadAllIssues = () => {
                     </span>
                 </div>
 
-                
                 <div class="mt-auto pt-4 border-t border-gray-50">
                     <p class="text-[11px] text-gray-500 font-medium mb-1">
                         #by ${issue.user_name}
@@ -59,11 +61,11 @@ const loadAllIssues = () => {
                         ${issue.updatedAt.split('T')[0]}
                     </p>
                 </div>
-
             </div>
-        </div>
-            `;
-           issuesCard.innerHTML += card;
+        `;
+
+       
+        issuesCard.appendChild(card);
         })
     }
 
