43ee747fbe94a1400e09a343698247ce411457a5
arafat33498-prog <arafat33598@gmail.com>
2026-03-06 23:32:45 +0600

    add issuesCard funtion in js

--- script/load.js
+++ script/load.js
@@ -5,8 +5,31 @@ const loadAllIssues = () => {
     .then((res) => res.json())
     .then ((data) =>{
         allIssues = data.data
-        console.log(allIssues)
+        console.log(allIssues);
+        displayIssues(allIssues)
     })
 
 }
+    const displayIssues = (issues) => {
+        const issuesCard = document.getElementById("issues-card")
+        issuesCard.innerHTML = ""
+
+        issues.forEach(issue => {
+            const card = `
+            <div class="card bg-base-100 shadow-xl border">
+                <div class="card-body">
+                    <h2 class="card-title text-lg">${issue.title}</h2>
+                    <p class="text-sm text-gray-600">${issue.description}</p>
+                    <div class="card-actions justify-end mt-4">
+                        <div class="badge badge-outline">${issue.status}</div>
+                        <div class="badge badge-primary">${issue.priority}</div>
+                    </div>
+                </div>
+            </div>
+            `;
+           issuesCard.innerHTML += card;
+        })
+    }
+
+
 loadAllIssues();
